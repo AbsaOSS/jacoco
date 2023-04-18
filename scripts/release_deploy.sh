681970284fd92ea3634dd8f8256a0cf27a139d0d
miroslavpojer <109138042+miroslavpojer@users.noreply.github.com>
2023-04-18 12:00:43 +0200

    Final changes after successful release. For release used newly added scripts.

--- scripts/release_deploy.sh
+++ scripts/release_deploy.sh
@@ -4,4 +4,5 @@ rm -rf jacoco_release
 git clone https://github.com/AbsaOSS/jacoco.git jacoco_release
 cd jacoco_release
 git checkout master
-mvn deploy -Ppublic -Psonatype-oss-release -PgenerateComponentPreload -DskipTests
+#mvn deploy -Ppublic -Psonatype-oss-release -PgenerateComponentPreload -DskipTests
+mvn deploy -Ppublic -Psonatype-oss-release -DskipTests
